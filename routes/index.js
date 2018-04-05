fb80575c8ad238412aee1f6b6bfbd530d9c9b645
dererumnatura <pollindrom@gmail.com>
2018-04-05 21:37:49 +0300

    For diana

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,63 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { 
-    title: 'Form Validation', 
-    success: req.session.success, 
-    errors: req.session.errors 
-  });
-  req.session.errors = null;
+  res.render('index');
 });
 
-router.post('/submit', function(req, res, next) {
-  req.check('email', 'Invalid email adress').isEmail();
-  req.check('password', 'Password is invalid').isLength({min: 4}).equals(req.body.confirmPassword);
+router.get('/get-data', function(req, res, next) {
+  var resultArray = [];
+  /////////////////////
+  console.log('sdfsdf');
+  ///////////////////
+  mongo.connect(url, (err, database) => {
+    
+    assert.equal(null, err);
+    var dbTest = database.db('test');
+    var cursor = dbTest.collection('user-data').find();
+    cursor.forEach(function(doc, err) {
+      assert.equal(null, err);
+      resultArray.push(doc);
+    }, function() {
+      database.close();
+      res.render('index', {items: resultArray});
+    });
+  });
+});
 
-  var errors = req.validationErrors();
-  if (errors) {
-    req.session.errors = errors;
-    req.session.success = false;
-  } else {
-    req.session.cookie.success = true;
-  }
+router.post('/insert', function(req, res, next) {
+  var item = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author
+  };
+
+  mongo.connect(url, function(err, client) {
+    // console.log('Post first' + db); /////////////////////////////////////////////////////
+    var db = client.db('test');
+    assert.equal(null, err);
+    db.collection('user-data').insertOne(item, function(err, result) {
+      // console.log('Post secound' + db); /////////////////////////////////////////////////////
+      assert.equal(null, err);
+      // console.log('Item inserted');
+      client.close();
+    });
+  });
   res.redirect('/');
 });
 
+router.post('/update', function(req, res, next) {
+  
+});
 
+router.post('/delete', function(req, res, next) {
+  
+});
 
 module.exports = router;
